6c962954e83cb2c7571f241d2a5ce89bdfd738e3
Mithun <ms2921@ic.ac.uk>
2022-06-02 15:47:38 +0100

    Updated macro for emulator

--- src/emulate.h
+++ src/emulate.h
@@ -1,1 +1,12 @@
+#include <stdint.h>
+
 #define MEMORY_CAPACITY 65536
+#define NUM_REGISTERS 17
+#define PC_INDEX 15
+#define CPSR_INDEX 16
+#define NUM_GEN_PURPOSE_REGISTERS 13
+
+typedef struct {
+    uint32_t *registers;
+    /* TODO: Memory */
+} machine_state;
