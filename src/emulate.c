a54e178a0ee8efdb1606f1b0ccafe71c5deabf69
Mithun <ms2921@ic.ac.uk>
2022-06-02 15:07:21 +0100

    Reads binary file and closes.

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
+  FILE *file = fopen(argv[1], "rb");
+
+  /* Test for errors with file */
+
+  /* Initialise memory and registers to 0 */
+
+  /* Load instructions into memory */
+  
+  fclose(file);
+  /* Begin execution */
+
   return EXIT_SUCCESS;
 }
