59603d33e3e5e2e57294504469ed107c5fde462d
Taeho Kim <tk3718@ic.ac.uk>
2022-06-05 23:03:36 +0100

    Included Shewta's implementation in header file

--- src/data_processing.h
+++ src/data_processing.h
@@ -1,6 +1,36 @@
 #ifndef DATA_PROCESSING_H
 #define DATA_PROCESSING_H
 
+#include <stdio.h>
+#include <stdint.h>
+
+#define COND_OFFSET 28
+#define BIT_27_MASK 0x8000000
+#define BIT_27_OFFSET 27
+#define BIT_26_MASK 0x4000000
+#define BIT_26_OFFSET 26
+#define I_BIT_MASK 0x2000000
+#define I_BIT_OFFSET 25
+#define OPCODE_MASK 0x1e00000
+#define OPCODE_OFFSET 21
+#define S_BIT_MASK 0x100000
+#define S_BIT_OFFSET 20
+#define RN_MASK 0xf0000
+#define RN_OFFSET 16
+#define RD_MASK 0xf000
+#define RD_OFFSET 12
+#define OPERAND_2_MASK 0xfff
+#define OPERAND_2_IMMEDIATE_MASK 0xff
+#define ROTATE_OFFSET 8
+#define RM_MASK 0xf
+#define SHIFT_VALUE_MASK 0xff0
+#define SHIFT_OPTIONAL_BIT_MASK 0x10
+#define SHIFT_VALUE_OFFSET 4
+#define SHIFT_TYPE_MASK 0x6
+#define LAST_BIT_OFFSET 1
+#define SHIFT_AMOUNT_OFFSET 7
+#define SHIFT_LAST_BIT_MASK 0x1
+
 #define NUM_REGISTER_BITS 32
 #define MAX_STORE_VALUE (1 << 32 - 1)
 #define CARRY 1
@@ -32,6 +62,28 @@ typedef struct {
 	uint32_t value;
 } Operand2;
 
+int get_carry_bit(uint32_t);
+
+void and(int, int, uint32_t *);
+
+void eor(int, int, uint32_t *);
+
+void sub(int, int, uint32_t *);
+
+void rsb(int, int, uint32_t *);
+
+void add(int, int, uint32_t *);
+
+void tst(int, int);
+
+void teq(int, int);
+
+void cmp(int, int);
+
+void orr(int, int, uint32_t *);
+
+void mov(int, int, uint32_t *);
+
 Operand2 logical_left_shift(uint8_t, uint32_t);
 
 Operand2 logical_right_shift(uint8_t, uint32_t);
@@ -39,5 +91,7 @@ Operand2 logical_right_shift(uint8_t, uint32_t);
 Operand2 arithmetic_right_shift(uint8_t, uint32_t);
 
 Operand2 rotate_right(uint8_t, uint32_t);
+
+void manage(uint32_t, struct REGISTERS *);
 
 #endif
