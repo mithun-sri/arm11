8bdcd1a9e5be49140191fc13cd076189937730e9
Mithun <ms2921@ic.ac.uk>
2022-06-06 16:17:16 +0100

    Added functionality for transfer

--- src/single_data_transfer.c
+++ src/single_data_transfer.c
@@ -1,54 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int single_data_transfer(int l_bit, int offset_bit, int index_bit, int up_bit, int rn, uint16_t *offset, uint32_t *rd){
-    /* Check CPSR Condition */
-
-    /* Calculate offset */
-    if (offset_bit == 1){
-        /* Shifted register */
-    } else if (offset_bit == 0){
-        /* Immediate value */
-    }
-    
-    uint32_t base_register;
+int single_data_transfer(int l_bit, int offset_bit, int index_bit, int up_bit, uint32_t rn, uint32_t offset, uint32_t rd){
     /* Check pre/post indexing */
     if (index_bit == 1){
         /* Pre-indexing */
-        uint32_t new_address = compute_address(base_register, *offset, up_bit);
-        transfer(l_bit , new_address);
+        uint32_t new_address = compute_address(rn, offset, up_bit);
+        perform_transfer(l_bit , new_address, rd);
     } else if (index_bit == 0){
         /* Post-indexing */
-        transfer(l_bit, rn);
-        base_register = compute_address(base_register, *offset, up_bit);
+        perform_transfer(l_bit, rn, rd);
+        uint32_t new_address = compute_address(rn, offset, up_bit);
         /* Update base register */
-        register_write(base_register);
+        register_write(rn, new_address);
     }
+    return EXIT_SUCCESS;
 }
 
-void transfer(int l_bit, uint32_t new_address){
+void perform_transfer(int l_bit, uint32_t new_address, uint32_t rd){
     if (l_bit == 1){
         /* Load instruction */
-        /* Write to register */
+        register_write(rd, new_address);
     } else if (l_bit == 0){
         /* Store instruction */
-        /* Write to memory */
+        memory_write(rd, new_address);
     }
 }
 
-void register_write(uint32_t address){
+/* Write from memory to register */
+void register_write(uint32_t dest, uint32_t address){
 
 }
 
-void memory_write(uint32_t address){
+/* Write from register to memory */
+void memory_write(uint32_t src, uint32_t address){
 
 }
 
-uint32_t compute_address(uint32_t *rn, uint32_t *offset, int up_bit){
-    uint32_t new_address = 0;
+uint32_t compute_address(uint32_t value, uint32_t offset, int up_bit){
+    uint32_t address = 0;
     if (up_bit == 1){
-        new_address = rn + *offset;
+        address = value + offset;
     } else if (up_bit == 0){
-        new_address = rn - *offset;
+        address = value - offset;
     }
+    return address;
 }
