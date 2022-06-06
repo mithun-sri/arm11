bdaaacf8519549168771654c008acd9a146bc281
Mithun <ms2921@ic.ac.uk>
2022-06-06 12:57:47 +0100

    updated skeleton

--- src/single_data_transfer.c
+++ src/single_data_transfer.c
@@ -10,40 +10,45 @@ int single_data_transfer(int l_bit, int offset_bit, int index_bit, int up_bit, i
     } else if (offset_bit == 0){
         /* Immediate value */
     }
+    
+    uint32_t base_register;
+    /* Check pre/post indexing */
+    if (index_bit == 1){
+        /* Pre-indexing */
+        uint32_t new_address = compute_address(base_register, *offset, up_bit);
+        transfer(l_bit , new_address);
+    } else if (index_bit == 0){
+        /* Post-indexing */
+        transfer(l_bit, rn);
+        base_register = compute_address(base_register, *offset, up_bit);
+        /* Update base register */
+        register_write(base_register);
+    }
+}
 
-    uint32_t new_address;
+void transfer(int l_bit, uint32_t new_address){
+    if (l_bit == 1){
+        /* Load instruction */
+        /* Write to register */
+    } else if (l_bit == 0){
+        /* Store instruction */
+        /* Write to memory */
+    }
+}
 
+void register_write(uint32_t address){
+
+}
+
+void memory_write(uint32_t address){
+
+}
+
+uint32_t compute_address(uint32_t *rn, uint32_t *offset, int up_bit){
+    uint32_t new_address = 0;
     if (up_bit == 1){
         new_address = rn + *offset;
     } else if (up_bit == 0){
         new_address = rn - *offset;
     }
-
-    /* Check pre/post indexing */
-    if (index_bit == 1){
-        /* Pre-indexing */
-        transfer(l_bit);
-    } else if (index_bit == 0){
-        /* Post-indexing */
-        transfer(l_bit);
-    }
-}
-
-void transfer(int l_bit){
-    if (l_bit == 1){
-        /* Load instruction */
-        register_write();
-    } else if (l_bit == 0){
-        /* Store instruction */
-        memory_write();
-        register_write();
-    }
-}
-
-void register_write(){
-
-}
-
-void memory_write(){
-
 }
