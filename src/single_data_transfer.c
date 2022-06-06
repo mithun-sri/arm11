70653ed37aaa7f350ac9a84f4435baf6acc6f6f0
Mithun <ms2921@ic.ac.uk>
2022-06-06 11:54:35 +0100

    updated skeleton for singe_data_transfer

--- src/single_data_transfer.c
+++ src/single_data_transfer.c
@@ -11,10 +11,39 @@ int single_data_transfer(int l_bit, int offset_bit, int index_bit, int up_bit, i
         /* Immediate value */
     }
 
+    uint32_t new_address;
+
+    if (up_bit == 1){
+        new_address = rn + *offset;
+    } else if (up_bit == 0){
+        new_address = rn - *offset;
+    }
+
     /* Check pre/post indexing */
     if (index_bit == 1){
         /* Pre-indexing */
+        transfer(l_bit);
     } else if (index_bit == 0){
         /* Post-indexing */
+        transfer(l_bit);
     }
+}
+
+void transfer(int l_bit){
+    if (l_bit == 1){
+        /* Load instruction */
+        register_write();
+    } else if (l_bit == 0){
+        /* Store instruction */
+        memory_write();
+        register_write();
+    }
+}
+
+void register_write(){
+
+}
+
+void memory_write(){
+
 }
