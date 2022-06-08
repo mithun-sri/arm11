dcaa1b29e48cf608371ecfe475d3267e3476adc5
ms2921 <mithun.srinavaratnarajah21@imperial.ac.uk>
2022-06-08 19:21:44 +0000

    style update

--- src/multiply.c
+++ src/multiply.c
@@ -1,36 +1,31 @@
 #include "multiply.h"
 
 void multiply(uint32_t instruction, struct *registers r) {
-if (succeeds(instruction, r) == 1) {
+  if (succeeds(instruction, r) == 1) {
+    uint32_t accumulate = (instruction >> 21) & 1;
+    uint32_t set_conditions = (instructions >> 20) & 1;
   
-  uint32_t accumulate = (instruction >> 21) & 1;
-  uint32_t set_conditions = (instructions >> 20) & 1;
-  
-  uint32_t Rn = r.general[(instruction >> 12) & 15];
-  uint32_t Rs = r.general[(instruction >> 8) & 15];
-  uint32_t Rm = r.general[instruction & 15];
+    uint32_t rn = r.general[(instruction >> 12) & 15];
+    uint32_t rs = r.general[(instruction >> 8) & 15];
+    uint32_t rm = r.general[instruction & 15];
 
-  uint32_t register_to_write_to = (instruction >> 16) & 15;  // need some way of defining registers
-  uint32_t result;
+    uint32_t register_to_write_to = (instruction >> 16) & 15;  // need some way of defining registers
+    uint32_t result;
 
-  if (accumulate == 1) {
-    result = Rm * Rs + Rn;
-  
-  } else {
-    result = Rm * Rs;
+    if (accumulate == 1) {
+      result = rm * rs + rn;
+    } else {
+      result = rm * rs;
+    }
+    r.general[register_to_write_to] = result;
+
+    if (set_conditions == 1) {
+      if (result == 0) {
+        r.cpsr |= Z_MASK_CPSR;  
+    } else if (result < 0) {
+        r.cpsr |= N_MASK_CPSR;
+    }
   }
-
-  r.general[register_to_write_to] = result;
-
-
-  if (set_conditions == 1) {
-    if (result == 0) {
-      r.cpsr |= Z_MASK_CPSR;  
-  } else if (result < 0) {
-      r.cpsr |= N_MASK_CPSR;
-  }
-  }
-
 }
   
     
