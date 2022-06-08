6280fdd70640693241c7cee14f1a31b2f0d241ea
ms2921 <mithun.srinavaratnarajah21@imperial.ac.uk>
2022-06-08 18:27:26 +0000

    Update src/emulate.c Deleted src/instruction.c, src/emulate.h

--- src/emulate.c
+++ src/emulate.c
@@ -3,6 +3,20 @@
 #include "emulate.h"
 #define BYTES_PER_WORD 4
 #define MAX_INSTR_COUNT 16384
+#define MEMORY_CAPACITY 65536
+#define NUM_REGISTERS 17
+#define PC_INDEX 15
+#define CPSR_INDEX 16
+#define NUM_GEN_PURPOSE_REGISTERS 13
+#define BYTES_PER_WORD 4
+
+struct registers {
+	uint32_t *gen_regs[NUM_GEN_PURPOSE_REGISTERS];
+	uint32_t pc;
+	uint32_t cpsr;
+};
+
+uint8_t *memory;
 
 int main(int argc, char **argv) {
   FILE *file;
