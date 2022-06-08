9a09a85d430e43ecae26989641cab465df3c9adc
ms2921 <mithun.srinavaratnarajah21@imperial.ac.uk>
2022-06-08 19:07:29 +0000

    Update src/branch.c, src/branch.h

--- src/branch.c
+++ src/branch.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "emulate_utilities.h"
 
-#define SIGN_EXT_MASK 4227858432     // in binary: 11111100000000000000000000000000
+#include "branch.h"
 
+void branch(uint32_t instruction) {
 
-if (succeeds(instruction, r) == 1) {  // checks if conditions are met for the instruction based                                         on the contents of the register file, r.
-  uint32_t mask = ((1 << 24) - 1);
-  uint32_t offset = instruction & mask;
-  offset <<= 2;
+  if (succeeds(instruction, regs) == 1) {  // checks if conditions are met for the instruction based                                         on the contents of the register file, r.
+    uint32_t mask = ((1 << 24) - 1);
+    uint32_t offset = instruction & mask;
+    offset <<= 2;
 
-  if (((offset >> 24) & 1) == 1) {
-    offset |= SIGN_EXT_MASK;
+    if (((offset >> 24) & 1) == 1) {
+      offset |= SIGN_EXT_MASK;
+    }
+
+    uint32_t branched_instr_addr = regs.pc + offset;
+    regs.pc = branched_instr_addr;
   }
 
-  uint32_t branched_instr_addr = r.pc + offset;
-  registers.pc = branched_instr_addr;
 }
-    
+ 
---
+++ src/branch.h
@@ -0,0 +1,10 @@
+#ifndef BRANCH_H
+#define BRANCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "emulate_utilities.h"
+
+#define SIGN_EXT_MASK 4227858432     // in binary: 11111100000000000000000000000000
+
+#endif
