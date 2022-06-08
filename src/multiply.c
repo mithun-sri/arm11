4a1f8d0d596b6f1095d6e7145f6c823bdd74294c
Fernando, Devin <devin.fernando20@imperial.ac.uk>
2022-06-08 13:31:36 +0000

    multiply now a function

--- src/multiply.c
+++ src/multiply.c
@@ -12,35 +12,35 @@ void multiply(uint32_t instruction) {
 
   if (succeeds(instruction, r) == 1) {
   
-  uint32_t accumulate = (instruction >> 21) & 1;
-  uint32_t set_conditions = (instructions >> 20) & 1;
+    uint32_t accumulate = (instruction >> 21) & 1;
+    uint32_t set_conditions = (instructions >> 20) & 1;
   
-  uint32_t Rn = r.general[(instruction >> 12) & 15];
-  uint32_t Rs = r.general[(instruction >> 8) & 15];
-  uint32_t Rm = r.general[instruction & 15];
+    uint32_t Rn = r.general[(instruction >> 12) & 15];
+    uint32_t Rs = r.general[(instruction >> 8) & 15];
+    uint32_t Rm = r.general[instruction & 15];
 
-  uint32_t register_to_write_to = (instruction >> 16) & 15;  // need some way of defining registers
-  uint32_t result;
+    uint32_t register_to_write_to = (instruction >> 16) & 15;  // need some way of defining registers
+    uint32_t result;
 
-  if (accumulate == 1) {
-    result = Rm * Rs + Rn;
+    if (accumulate == 1) {
+      result = Rm * Rs + Rn;
   
-  } else {
-    result = Rm * Rs;
+    } else {
+      result = Rm * Rs;
+    }
+
+    r.general[register_to_write_to] = result;
+
+
+    if (set_conditions == 1) {
+      if (result == 0) {
+        r.cpsr |= Z_MASK_CPSR;  
+      } else if (result < 0) {
+        r.cpsr |= N_MASK_CPSR;
+      }
+    }
+
   }
-
-  r.general[register_to_write_to] = result;
-
-
-  if (set_conditions == 1) {
-    if (result == 0) {
-      r.cpsr |= Z_MASK_CPSR;  
-  } else if (result < 0) {
-      r.cpsr |= N_MASK_CPSR;
-  }
-  }
-
- }
 
 }
 
