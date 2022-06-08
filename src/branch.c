e3e4a39d01e5b11a35b0d25389959c85bbb4245e
Fernando, Devin <devin.fernando20@imperial.ac.uk>
2022-06-08 15:41:27 +0000

    changed header

--- src/branch.c
+++ src/branch.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "reg.c"
-#include "instruction.c"
-
-#define SIGN_EXT_MASK 4227858432     // in binary: 11111100000000000000000000000000
+#include <branch.h>
 
 void branch(uint32_t instruction) {
 
