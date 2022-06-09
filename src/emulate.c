c1e2adc5b4e84d39b12ecbf10e5d3d6e2c31085e
Mithun Sri <ms2921@vm-shell3.doc.ic.ac.uk>
2022-06-09 21:48:30 +0100

    fixed emulate.c

--- src/emulate.c
+++ src/emulate.c
@@ -1,7 +1,4 @@
 #include "emulate_architecture.h"
-<<<<<<< HEAD
-
-||||||| 5316504
 #include "choose_instruction.h"
 
 uint8_t *memory;
@@ -9,7 +6,6 @@ uint8_t *memory;
 =======
 #include "choose_instruction.h"
 
->>>>>>> 4a0cd6fae3a918076ce5df94fe7e609e15ad007a
 int main(int argc, char **argv) {
   FILE *file;
   if ((file = fopen(argv[1], "rb")) == NULL){
@@ -18,20 +14,7 @@ int main(int argc, char **argv) {
   }
 
   /* Initialise memory and registers to 0 */
-<<<<<<< HEAD
-  struct registers regs;
-  *regs.gen_regs = calloc(NUM_GEN_PURPOSE_REGISTERS, sizeof(uint32_t));
-  regs.cpsr = 0;
-  *regs.pc = 0;
-  *memory = 0;
-||||||| 5316504
-  struct registers regs;
-  *regs.gen_regs = calloc(NUM_GEN_PURPOSE_REGISTERS, sizeof(uint32_t));
-  regs.cpsr = 0;
-  regs.pc = 0;
-=======
   struct registers regs = { calloc(NUM_GEN_PURPOSE_REGISTERS, sizeof(uint32_t)), 0, 0};
->>>>>>> 4a0cd6fae3a918076ce5df94fe7e609e15ad007a
   memory = calloc(MEMORY_CAPACITY, sizeof(uint8_t));
 
   /* Load instruction */
