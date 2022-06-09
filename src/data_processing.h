9c49c147fdf26dcced2d657932732fbc2894f72d
Mithun <ms2921@ic.ac.uk>
2022-06-09 23:35:25 +0100

    Updated mask values

--- src/data_processing.h
+++ src/data_processing.h
@@ -6,32 +6,23 @@
 #include "emulate_utilities.h"
 #include "emulate_architecture.h"
 
-#define COND_OFFSET 28
-#define BIT_27_MASK 0x8000000
+#define LAST_FOUR_BITS_MASK 0xf
 #define BIT_27_OFFSET 27
-#define BIT_26_MASK 0x4000000
 #define BIT_26_OFFSET 26
-#define I_BIT_MASK 0x2000000
 #define I_BIT_OFFSET 25
-#define OPCODE_MASK 0x1e00000
 #define OPCODE_OFFSET 21
-#define S_BIT_MASK 0x100000
 #define S_BIT_OFFSET 20
-#define RN_MASK 0xf0000
 #define RN_OFFSET 16
-#define RD_MASK 0xf000
 #define RD_OFFSET 12
 #define OPERAND_2_MASK 0xfff
 #define OPERAND_2_IMMEDIATE_MASK 0xff
 #define ROTATE_OFFSET 8
-#define RM_MASK 0xf
-#define SHIFT_VALUE_MASK 0xff0
-#define SHIFT_OPTIONAL_BIT_MASK 0x10
 #define SHIFT_VALUE_OFFSET 4
-#define SHIFT_TYPE_MASK 0x6
-#define LAST_BIT_OFFSET 1
+#define RS_OFFSET 8
 #define SHIFT_AMOUNT_OFFSET 7
+#define SHIFT_TYPE_OFFSET 5
 #define LAST_BIT_MASK 0x1
+#define LAST_TWO_BITS_MASK 0x3
 #define CPSR_N_OFFSET 31
 #define CPSR_Z_OFFSET 30
 #define CPSR_C_OFFSET 29
@@ -42,7 +33,7 @@
 #define MOST_SIGNIFICANT 0x80000000
 #define MOST_SIGNIFICANT_OFFSET 31
 #define REGISTER_BITS 32
-#define REG_MAX_VALUE (0x100000000 - 1)
+#define LAST_BYTE_MASK 0xff
 
 typedef enum {
 	AND = 0,
