4136a51ac79bac034de25f890ac9dde744c3f5f9
ms2921 <mithun.srinavaratnarajah21@imperial.ac.uk>
2022-06-09 15:15:10 +0000

    Update src/multiply.h

--- src/multiply.h
+++ src/multiply.h
@@ -2,6 +2,7 @@
 #define MULTIPLY_H
 #include <stdint.h>
 #include "emulate_utilities.h"
+#include "emulate_architecture.h"
 
 #define Z_MASK_CPSR 1073741824  // 0100...00 in 32 bit binary
 #define N_MASK_CPSR 2147483648  // 1000...00 in 32 bit binary
