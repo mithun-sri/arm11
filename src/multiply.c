99797b32b6ccd6ac49912773ada3988c95bad81a
Fernando, Devin <devin.fernando20@imperial.ac.uk>
2022-06-09 22:28:54 +0000

    Update src/multiply.c

--- src/multiply.c
+++ src/multiply.c
@@ -1,13 +1,13 @@
 #include "multiply.h"
 
-void multiply(uint32_t instruction, struct registers r) {
-  if (succeeds(instruction, r) == 1) {
+void multiply(uint32_t instruction, struct registers regs) {
+  if (succeeds(instruction, regs) == 1) {
     uint32_t accumulate = (instruction >> 21) & 1;
     uint32_t set_conditions = (instruction >> 20) & 1;
   
-    uint32_t rn = *r.gen_regs[(instruction >> 12) & 15];
-    uint32_t rs = *r.gen_regs[(instruction >> 8) & 15];
-    uint32_t rm = *r.gen_regs[instruction & 15];
+    uint32_t rn = *regs.gen_regs[(instruction >> 12) & 15];
+    uint32_t rs = *regs.gen_regs[(instruction >> 8) & 15];
+    uint32_t rm = *regs.gen_regs[instruction & 15];
 
     uint32_t register_to_write_to = (instruction >> 16) & 15;  // need some way of defining registers
     uint32_t result;
@@ -17,7 +17,7 @@ void multiply(uint32_t instruction, struct registers r) {
     } else {
       result = rm * rs;
     }
-    *r.gen_regs[register_to_write_to] = result;
+    *regs.gen_regs[register_to_write_to] = result;
 
     if (set_conditions == 1) {
       if (result == 0) {
