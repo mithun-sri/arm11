e723fc8777984c6d8ec1c2392c99cde435593d9f
Fernando, Devin <devin.fernando20@imperial.ac.uk>
2022-06-09 10:29:29 +0000

    added function def

--- src/multiply.h
+++ src/multiply.h
@@ -5,3 +5,5 @@
 
 #define Z_MASK_CPSR 1073741824  // 0100...00 in 32 bit binary
 #define N_MASK_CPSR 2147483648  // 1000...00 in 32 bit binary
+
+void multiply(uint32_t instruction, struct registers regs);
