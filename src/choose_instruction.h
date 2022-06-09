73b51ed66f0148f76c78deda440e7c12b89cc107
Fernando, Devin <devin.fernando20@imperial.ac.uk>
2022-06-09 10:48:43 +0000

    Added File

---
+++ src/choose_instruction.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct data_pipeline {
+  uint32_t fetched;
+  uint32_t decoded;
+  uint8_t fetch_set;
+  uint8_t decode_set;
+  uint8_t instr_set;
+};
+
+void execute_instr(uint32_t instruction, struct registers regs);
+
+void run_emulator(struct registers regs);
