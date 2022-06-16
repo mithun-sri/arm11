18a546ca17a4386b7b9d221c1260a30f8f18331b
Banerjee, Shweta <shweta.banerjee21@imperial.ac.uk>
2022-06-16 22:31:21 +0000

    updated tokenizer to call mov/add function - need to optimize + tidy up + complete

--- src/assemble.c
+++ src/assemble.c
@@ -65,72 +65,67 @@ Q - How do you generate rotate value? Is it ok to just say rotate == 0 each time
 #define ADD_OPCODE 0x4
 #define ORR_OPCODE 0xc
 #define COND 0xe
-#define S_BIT_MASK 1 << S_BIT_OFFSET
+#define S_BIT_MASK (1 << S_BIT_OFFSET)
 // not implementing shifted register
 #define I_BIT 0x1
-#define COMMON_DATA_PROCESSING_BITS (COND << COND_OFFSET + I_BIT << I_BIT_OFFSET)
+#define COMMON_DATA_PROCESSING_BITS ((COND << COND_OFFSET) + (I_BIT << I_BIT_OFFSET))
 // no line is longer than 511 characters - spec
 #define MAX_CHARS 511
 
-uint32_t add_a(uint32_t rn, uint32_t rd, Operand2 op2) {
-  return (COMMON_DATA_PROCESSING_BITS \ 
-    + (ADD_OPCODE << OPCODE_OFFSET) + (rn << RN_OFFSET) + (rd << RD_OFFSET) \ 
-    + op2.value);
+uint32_t add_a(uint8_t rn, uint8_t rd, uint8_t op2) {
+  return (COMMON_DATA_PROCESSING_BITS + (ADD_OPCODE << OPCODE_OFFSET) 
+  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
-uint32_t sub_a(uint32_t rn, uint32_t rd, Operand2 op2) {
-  return (COMMON_DATA_PROCESSING_BITS \ 
-    + (SUB_OPCODE << OPCODE_OFFSET) + (rn << RN_OFFSET) + (rd << RD_OFFSET) \ 
-    + op2.value);
+uint32_t sub_a(uint8_t rn, uint8_t rd, uint8_t op2) {
+  return (COMMON_DATA_PROCESSING_BITS + (SUB_OPCODE << OPCODE_OFFSET) 
+  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
-uint32_t rsb_a(uint32_t rn, uint32_t rd, Operand2 op2) {
-  return (COMMON_DATA_PROCESSING_BITS \ 
-    + (RSB_OPCODE << OPCODE_OFFSET) + (rn << RN_OFFSET) + (rd << RD_OFFSET) \ 
-    + op2.value);
+uint32_t rsb_a(uint8_t rn, uint8_t rd, uint8_t op2) {
+  return (COMMON_DATA_PROCESSING_BITS + (RSB_OPCODE << OPCODE_OFFSET) 
+  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
-uint32_t and_a(uint32_t rn, uint32_t rd, Operand2 op2) {
-  return (COMMON_DATA_PROCESSING_BITS \ 
-    + (AND_OPCODE << OPCODE_OFFSET) + (rn << RN_OFFSET) + (rd << RD_OFFSET) \ 
-    + op2.value);
+uint32_t and_a(uint8_t rn, uint8_t rd, uint8_t op2) {
+  return (COMMON_DATA_PROCESSING_BITS + (AND_OPCODE << OPCODE_OFFSET) 
+  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
-uint32_t eor_a(uint32_t rn, uint32_t rd, Operand2 op2) {
-  return (COMMON_DATA_PROCESSING_BITS \ 
-    + (AND_OPCODE << OPCODE_OFFSET) + (rn << RN_OFFSET) + (rd << RD_OFFSET) \ 
-    + op2.value);
+uint32_t eor_a(uint8_t rn, uint8_t rd, uint8_t op2) {
+  return (COMMON_DATA_PROCESSING_BITS + (AND_OPCODE << OPCODE_OFFSET) 
+  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
-uint32_t orr_a(uint32_t rn, uint32_t rd, Operand2 op2) {
-  return (COMMON_DATA_PROCESSING_BITS \ 
-    + (AND_OPCODE << OPCODE_OFFSET) + (rn << RN_OFFSET) + (rd << RD_OFFSET) \ 
-    + op2.value);
+uint32_t orr_a(uint8_t rn, uint8_t rd, uint8_t op2) {
+  return (COMMON_DATA_PROCESSING_BITS + (AND_OPCODE << OPCODE_OFFSET) 
+  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
-int mov_a(uint8_t* rd, Operand2 op2) { 
-  return (COMMON_DATA_PROCESSING_BITS \
-    + (MOV_OPCODE << OPCODE_OFFSET) + (((long int) rd) << RD_OFFSET) \
-    + (op2.value));
+uint32_t mov_a(uint8_t rd, uint8_t op2) { 
+  return (COMMON_DATA_PROCESSING_BITS + (MOV_OPCODE << OPCODE_OFFSET) 
+  + (((long int) rd) << RD_OFFSET) + op2);
 }
 
-int tst_a(uint32_t rn, Operand2 op2) { 
-  return (COMMON_DATA_PROCESSING_BITS \
-    + (TST_OPCODE << OPCODE_OFFSET) + S_BIT_MASK \
-    + (rn << RN_OFFSET) + (op2.value)); 
+uint32_t tst_a(uint8_t rn, uint8_t op2) { 
+  return (COMMON_DATA_PROCESSING_BITS + (TST_OPCODE << OPCODE_OFFSET) 
+  + S_BIT_MASK + (rn << RN_OFFSET) + op2); 
 }
 
-int teq_a(Opcode opcode, uint32_t rn, Operand2 op2) {
-  return (COMMON_DATA_PROCESSING_BITS \
-    + (TEQ_OPCODE << OPCODE_OFFSET) + S_BIT_MASK \
-    + (rn << RN_OFFSET) + (op2.value)); 
+uint32_t teq_a(uint8_t rn, uint8_t op2) {
+  return (COMMON_DATA_PROCESSING_BITS + (TEQ_OPCODE << OPCODE_OFFSET) 
+  + S_BIT_MASK + (rn << RN_OFFSET) + op2); 
 }
 
-int cmp_a(Opcode opcode, uint32_t rn, Operand2 op2) {
+uint32_t cmp_a(uint8_t rn, uint8_t op2) {
   uint8_t s_bit = 1;
-  return (COMMON_DATA_PROCESSING_BITS \
-    + (CMP_OPCODE << OPCODE_OFFSET) + S_BIT_MASK \
-    + (rn << RN_OFFSET) + (op2.value));
+  return (COMMON_DATA_PROCESSING_BITS + (CMP_OPCODE << OPCODE_OFFSET) 
+  + S_BIT_MASK + (rn << RN_OFFSET) + op2);
+}
+
+uint8_t get_val(char *str[MAX_CHARS], uint8_t pos) {
+  str[pos] = str[pos] + 1;
+  return atoi(str[pos]);
 }
 
 void tokenize(char instruction[]) {
@@ -138,20 +133,45 @@ void tokenize(char instruction[]) {
   char delimit[] = " ,";
   int i = 0;
   
-  str[i] = strtok(instruction,delimit);
-    while(str[i] != NULL)                    
-    {
-      printf("word %d : %s\n", i, str[i]);
-      i++;
-      str[i] = strtok(NULL, delimit);
-      // for (int j = 0; j < sizeof(str); j++) {
-      //   printf("Printing str value %d : %p", j, str[j]);
-      // }
-    }
+  str[i] = strtok(instruction, delimit);
+  while(str[i] != NULL) {
+    i++;
+    str[i] = strtok(NULL, delimit);
+  }
+
+  if (!strcmp(str[0], "mov")) {
+  //   // remove r
+  //   uint8_t rd = get_val(str, 1);
+  //   printf("mm: %d\n", rd);
+
+  //   // remove #
+  //   uint8_t op2 = get_val(str, 2);
+  //   printf("mmm: %d\n", op2);
+
+  // printf("%d\n", mov_a(get_val(str, 1), get_val(str, 2)));
+
+    mov_a(get_val(str, 1), get_val(str, 2));
+  } else if (!strcmp(str[0], "add")) {
+    // remove r from rd
+    str[1] = str[1] + 1;
+    uint8_t rd = atoi(str[1]);
+    printf("a1: %d\n", rd);
+
+    // remove r from rn
+    str[2] = str[2] + 1;
+    uint8_t rn = atoi(str[2]);
+    printf("a: %d\n", rn);
+    
+    // remove # from op2
+    str[3] = str[3] + 1;
+    uint8_t op2 = atoi(str[3]);
+    printf("a: %d\n", op2);
+
+    printf("a: %d\n", add_a(rn, rd, op2));
+  }
 }
 
 int main(void) {
-  printf("I am working!\n");
   char instruction[511];
   FILE *fPtr;
 
