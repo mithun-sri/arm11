14ec9a5be3ded25391554b8a5d51f0759c2b3ad9
Taeho Kim <tk3718@ic.ac.uk>
2022-06-17 11:18:41 +0100

    Added Special Instructions

---
+++ src/special_assemble.c
@@ -0,0 +1,10 @@
+#include "special_assemble.h"
+
+uint32_t lsl_a(uint8_t rn, uint8_t op2) {
+    return (COND << COND_OFFSET) + (MOV_OPCODE << OPCODE_OFFSET) 
+    + (rn << RD_OFFSET) + (op2 << 7) + rn;
+}
+
+uint32_t andeq_a() {
+    return HALT;
+}
---
+++ src/special_assemble.h
@@ -0,0 +1,13 @@
+#ifndef SPECIAL_ASSEMBLE_H
+#define SPECIAL_ASSEMBLE_H
+
+#include <stdint.h>
+#include "data_processing_assemble.h"
+
+#define HALT 0x00000000
+
+uint32_t lsl_a(uint8_t rn, uint8_t op2);
+
+uint32_t andeq_a();
+
+#endif
