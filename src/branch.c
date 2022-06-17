746f356cf9aa9ef0f87302410098ab7b9d12cbde
Fernando, Devin <devin.fernando20@imperial.ac.uk>
2022-06-17 14:38:47 +0000

    newOffset introduced

--- src/branch.c
+++ src/branch.c
@@ -3,16 +3,18 @@
 struct registers branch(uint32_t instruction, struct registers regs) {
 
   if (succeeds(instruction, regs) == 1) {
-    int32_t offset = extract_bits(instruction, 0, 24);
+    uint32_t offset = extract_bits(instruction, 0, 24);
     offset <<= 2;
 
-    if (extract_bits(offset, 24, 25) == 1) {
+    if (extract_bits(offset, 25, 26) == 1) {
 	    printf("Enters\n");
-      offset |= SIGN_EXT_MASK;
+      int32_t new_offset = offset | SIGN_EXT_MASK;
+    } else {
+      int32_t new_offset = offset
     }
 
-    uint32_t branched_instr_addr = *(regs.pc) + offset;
-    printf("Offset: %i\n", offset);
+    uint32_t branched_instr_addr = *(regs.pc) + new_offset;
+    printf("Offset: %i\n", new_offset);
     printf("branched pos: %i\n", branched_instr_addr);
     printf("Previous PC: %i\n", *regs.pc);
     *regs.pc = branched_instr_addr;
