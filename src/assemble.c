6c130c9b0d8bd41e836f19661a30ea03d13e4243
Taeho Kim <tk3718@ic.ac.uk>
2022-06-17 13:39:45 +0100

    Removed data processing functions

--- src/assemble.c
+++ src/assemble.c
@@ -52,76 +52,8 @@ Q - How do you generate rotate value? Is it ok to just say rotate == 0 each time
 #include <stdio.h>
 #include <string.h>
 #include "emulate_architecture.h"
-#include "data_processing.h"
 
-#define MOV_OPCODE 0xd
-#define TST_OPCODE 0x8
-#define TEQ_OPCODE 0x9
-#define CMP_OPCODE 0xa
-#define AND_OPCODE 0x0
-#define EOR_OPCODE 0x1
-#define SUB_OPCODE 0x2
-#define RSB_OPCODE 0x3
-#define ADD_OPCODE 0x4
-#define ORR_OPCODE 0xc
-#define COND 0xe
-#define S_BIT_MASK (1 << S_BIT_OFFSET)
-// not implementing shifted register
-#define I_BIT 0x1
-#define COMMON_DATA_PROCESSING_BITS ((COND << COND_OFFSET) + (I_BIT << I_BIT_OFFSET))
-// no line is longer than 511 characters - spec
-#define MAX_CHARS 511
-
-uint32_t add_a(uint8_t rn, uint8_t rd, uint8_t op2) {
-  return (COMMON_DATA_PROCESSING_BITS + (ADD_OPCODE << OPCODE_OFFSET) 
-  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
-}
-
-uint32_t sub_a(uint8_t rn, uint8_t rd, uint8_t op2) {
-  return (COMMON_DATA_PROCESSING_BITS + (SUB_OPCODE << OPCODE_OFFSET) 
-  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
-}
-
-uint32_t rsb_a(uint8_t rn, uint8_t rd, uint8_t op2) {
-  return (COMMON_DATA_PROCESSING_BITS + (RSB_OPCODE << OPCODE_OFFSET) 
-  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
-}
-
-uint32_t and_a(uint8_t rn, uint8_t rd, uint8_t op2) {
-  return (COMMON_DATA_PROCESSING_BITS + (AND_OPCODE << OPCODE_OFFSET) 
-  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
-}
-
-uint32_t eor_a(uint8_t rn, uint8_t rd, uint8_t op2) {
-  return (COMMON_DATA_PROCESSING_BITS + (AND_OPCODE << OPCODE_OFFSET) 
-  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
-}
-
-uint32_t orr_a(uint8_t rn, uint8_t rd, uint8_t op2) {
-  return (COMMON_DATA_PROCESSING_BITS + (AND_OPCODE << OPCODE_OFFSET) 
-  + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
-}
-
-uint32_t mov_a(uint8_t rd, uint8_t op2) { 
-  return (COMMON_DATA_PROCESSING_BITS + (MOV_OPCODE << OPCODE_OFFSET) 
-  + (((long int) rd) << RD_OFFSET) + op2);
-}
-
-uint32_t tst_a(uint8_t rn, uint8_t op2) { 
-  return (COMMON_DATA_PROCESSING_BITS + (TST_OPCODE << OPCODE_OFFSET) 
-  + S_BIT_MASK + (rn << RN_OFFSET) + op2); 
-}
-
-uint32_t teq_a(uint8_t rn, uint8_t op2) {
-  return (COMMON_DATA_PROCESSING_BITS + (TEQ_OPCODE << OPCODE_OFFSET) 
-  + S_BIT_MASK + (rn << RN_OFFSET) + op2); 
-}
-
-uint32_t cmp_a(uint8_t rn, uint8_t op2) {
-  uint8_t s_bit = 1;
-  return (COMMON_DATA_PROCESSING_BITS + (CMP_OPCODE << OPCODE_OFFSET) 
-  + S_BIT_MASK + (rn << RN_OFFSET) + op2);
-}
+#define MAX_CHARS 511 // no line is longer than 511 characters - spec
 
 uint8_t get_val(char *str[MAX_CHARS], uint8_t pos) {
   str[pos] = str[pos] + 1;
