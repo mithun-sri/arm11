3c456b5d8cb3b67011fe15a8d00651762723f935
Fernando, Devin <devin.fernando20@imperial.ac.uk>
2022-06-17 11:40:38 +0000

    actually moved macros into header file. Mistake last time

--- src/multiply_assembler.c
+++ src/multiply_assembler.c
@@ -1,17 +1,5 @@
 #include "multiply_assembler.h"
 
-#include <stdint.h>
-
-#define MULTIPLY_COMMON_BITS (9 << 4)  // the 1001 in bits 4-7 are common to both multiply instructions
-#define RS_SHIFT 8
-#define RN_SHIFT 12
-#define RD_SHIFT 16
-#define S_BIT 0
-#define S_BIT_SHIFT 20
-#define A_BIT_SHIFT 21
-#define MUL_COND_CODE 14
-#define COND_SHIFT 28
-
 
 uint32_t mla_a(uint8_t Rd, uint8_t Rm, uint8_t Rs, uint8_t Rn) {
   return (MULTIPLY_COMMON_BITS + Rm + (Rs << RS_SHIFT) + (Rn << RN_SHIFT) + (Rd << RD_SHIFT) + (S_BIT << S_BIT_SHIFT) + (1 << A_BIT_SHIFT) + (MUL_COND_CODE << COND_SHIFT));
--- src/multiply_assembler.h
+++ src/multiply_assembler.h
@@ -1,6 +1,16 @@
 #include <stdint.h>
 
 
+#define MULTIPLY_COMMON_BITS (9 << 4)  // the 1001 in bits 4-7 are common to both multiply instructions
+#define RS_SHIFT 8
+#define RN_SHIFT 12
+#define RD_SHIFT 16
+#define S_BIT 0
+#define S_BIT_SHIFT 20
+#define A_BIT_SHIFT 21
+#define MUL_COND_CODE 14
+#define COND_SHIFT 28
+
 uint32_t mla_a(uint8_t Rd, uint8_t Rm, uint8_t Rs, uint8_t Rn);
 
 uint32_t mul_a(uint8_t Rd, uint8_t Rm, uint8_t Rs);
