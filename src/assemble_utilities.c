9abca7ddd307f8a4dc127c0850870beeca510fc3
Taeho Kim <tk3718@ic.ac.uk>
2022-06-17 15:55:18 +0100

    Typo fixed

--- src/assemble_utilities.c
+++ src/assemble_utilities.c
@@ -1,7 +1,7 @@
 #include "assemble_utilities.h"
 
 uint32_t big_endian_to_little_endian(uint32_t big_endian) {
-    return ((big_endian >> ONE_BYTE_SHIFT) & BYTE_ZERO_MASK)  
+    return ((big_endian >> THREE_BYTE_SHIFT) & BYTE_ZERO_MASK)  
         | ((big_endian >> ONE_BYTE_SHIFT) & BYTE_ONE_MASK)
         | ((big_endian << ONE_BYTE_SHIFT) & BYTE_TWO_MASK) 
         | ((big_endian << THREE_BYTE_SHIFT) & BYTE_THREE_MASK);
