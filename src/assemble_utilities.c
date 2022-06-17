82bf299cdebef5451f9c3fb17b37d3e6c6d57c3b
Taeho Kim <tk3718@ic.ac.uk>
2022-06-17 15:06:52 +0100

    Fixed Format

--- src/assemble_utilities.c
+++ src/assemble_utilities.c
@@ -4,7 +4,7 @@ uint32_t big_endian_to_little_endian(uint32_t big_endian) {
     return ((big_endian >> ONE_BYTE_SHIFT) & BYTE_ZERO_MASK)  
         | ((big_endian >> ONE_BYTE_SHIFT) & BYTE_ONE_MASK)
         | ((big_endian << ONE_BYTE_SHIFT) & BYTE_TWO_MASK) 
-        | ((big_endian<<THREE_BYTE_SHIFT) & BYTE_THREE_MASK);
+        | ((big_endian << THREE_BYTE_SHIFT) & BYTE_THREE_MASK);
 }
 
 char *binary_int_to_chars(uint32_t little_endian) {
