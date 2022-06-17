81498112c9f005313f8c1026c73f0d4aac394f30
Taeho Kim <tk3718@ic.ac.uk>
2022-06-17 14:52:44 +0100

    Added function converting binary number to string

--- src/assemble_utilities.c
+++ src/assemble_utilities.c
@@ -7,3 +7,17 @@ uint32_t big_endian_to_little_endian(uint32_t big_endian) {
         | ((big_endian<<THREE_BYTE_SHIFT) & BYTE_THREE_MASK);
 }
 
+char *binary_int_to_chars(uint32_t little_endian) {
+    uint32_t num = little_endian;
+    char *binary_number = malloc(sizeof(char) * NUMBER_OF_INSTRUCTION_BITS);
+
+    for (int i=1; i<=NUMBER_OF_INSTRUCTION_BITS; i++) {
+        if (num % 2) {
+            binary_number[NUMBER_OF_INSTRUCTION_BITS-i] = ONE;
+        } else {
+            binary_number[NUMBER_OF_INSTRUCTION_BITS-i] = ZERO;
+        }
+        num = num / 2;
+    }
+    return binary_number;
+}
--- src/assemble_utilities.h
+++ src/assemble_utilities.h
@@ -6,8 +6,12 @@
 #define BYTE_TWO_MASK 0xff0000
 #define BYTE_THREE_MASK 0xff000000
 #define ONE_BYTE_SHIFT 0x8
-#define THREE_BYTE_SHIFT 0x24
+#define THREE_BYTE_SHIFT 0x18
+#define NUMBER_OF_INSTRUCTION_BITS 0x20
+#define ONE '1'
+#define ZERO '0'
 
+uint32_t big_endian_to_little_endian(uint32_t big_endian);
 
-uint32_t big_endian_to_little_endian(uint32_t little_endian);
+char *binary_int_to_chars(uint32_t little_endian);
 
