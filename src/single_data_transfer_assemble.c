d73ad377b3b34565c4b98e18223a7e8d0016d32b
Mithun Sri <ms2921@vm-shell3.doc.ic.ac.uk>
2022-06-24 12:41:43 +0100

    Fixed magic numbers in single data transfer

--- src/single_data_transfer_assemble.c
+++ src/single_data_transfer_assemble.c
@@ -3,31 +3,16 @@
 uint32_t find_up_bit(int8_t offset){
     uint32_t up_bit;
     if (offset >= 0){
-        up_bit = (1 << 23);
+        up_bit = (1 << UP_BIT_INDEX);
     } else {
         up_bit = 0;
     }
     return up_bit;
 }
 
-uint32_t convert(uint8_t n) {
-  uint32_t bin = 0;
-  int rem, i = 1;
-
-  while (n!=0) {
-    rem = n % 2;
-    n /= 2;
-    bin += rem * i;
-    i *= 10;
-  }
-
-  return bin;
-}
-
 uint32_t ldr_a(char** str){
     if (str[3] == NULL){
         // Pre-index without offset or immediate
-        // printf("Pre-index without offset or immediate\n");
         if (strncmp(str[2], "[", 1) == 0){
             // Pre-index without offset
             char* value_str = str[2];
@@ -35,14 +20,12 @@ uint32_t ldr_a(char** str){
             value_str += 1;
             uint32_t rn = atoi(value_str + 1) << 16;
             uint32_t rd = atoi(str[1] + 1) << 12;
-            uint32_t cond = 0xE << 28;
-            uint32_t standard_bits = 1 << 26;
             uint32_t i_bit = 0;
-            uint32_t p_bit = 1 << 24;
-            uint32_t l_bit = 1 << 20;
+            uint32_t p_bit = 1 << P_BIT_INDEX;
+            uint32_t l_bit = 1 << L_BIT_INDEX;
             uint32_t offset = 0;
-            uint32_t u_bit = 1 << 23;
-            return (rn | rd | cond | standard_bits | u_bit | i_bit | p_bit | l_bit | offset);
+            uint32_t u_bit = 1 << UP_BIT_INDEX;
+            return (rn | rd | COND_MASK | STANDARD_BIT_MASK | u_bit | i_bit | p_bit | l_bit | offset);
         } else {
             // Immediate
             char* value_str = str[2];
@@ -55,15 +38,14 @@ uint32_t ldr_a(char** str){
             }
 
             if (val > 0xFF){
-                // To do
                 uint32_t u_bit;
                 if (val >= 0){
                     u_bit = 1;
                 } else {
                     u_bit = 0;
                 }
-            uint32_t value = (uint32_t) val;
-            return (u_bit | value);
+                uint32_t value = (uint32_t) val;
+                return (u_bit | value);
             } else {
                 uint32_t rd = atoi(str[1] + 1) << 12;
                 return mov_a(rd, value_str + 1);
@@ -89,31 +71,28 @@ uint32_t ldr_a(char** str){
                 s_offset = strtol(&val_str[2], NULL, 16);
                 u_bit = 0;
             } else {
-                u_bit = 1 << 23;
+                u_bit = 1 << UP_BIT_INDEX;
             }
             uint32_t rn = atoi(str[2] + 2) << 16;
             uint32_t rd = atoi(str[1] + 1) << 12;
-            uint32_t cond = 0xE << 28;
-            uint32_t standard_bits = 1 << 26;
             uint32_t i_bit = 0;
-            uint32_t p_bit = 1 << 24;
-            uint32_t l_bit = 1 << 20;
+            uint32_t p_bit = 1 << P_BIT_INDEX;
+            uint32_t l_bit = 1 << L_BIT_INDEX;
             uint32_t offset = s_offset;
-            return (rn | rd | cond | standard_bits | i_bit | p_bit | l_bit | offset | u_bit);
+            return (rn | rd | COND_MASK | STANDARD_BIT_MASK | i_bit | p_bit | l_bit | offset | u_bit);
         } else {
             // Post-index
             uint32_t offset = atoi(str[3]);
             uint32_t i_bit = 1 << 25;
-            uint32_t l_bit = 1 << 20;
+            uint32_t l_bit = 1 << L_BIT_INDEX;
             char* rn_str = str[2] + 2;
             uint32_t rn = atoi(rn_str) << 16;
-            uint32_t cond = 0xE << 28;
-            uint32_t p_bit = 0 << 24;
-            uint32_t u_bit = 1 << 23;
+            uint32_t p_bit = 0 << P_BIT_INDEX;
+            uint32_t u_bit = 1 << UP_BIT_INDEX;
             int rd = atoi(str[1] + 1);
             rd <<= 12;
             uint32_t rd_mask = *(uint32_t*)&rd;
-            uint32_t res = (p_bit | i_bit | cond | u_bit | l_bit | rn | rd_mask | offset | (1 << 26));
+            uint32_t res = (p_bit | i_bit | COND_MASK | u_bit | l_bit | rn | rd_mask | offset | STANDARD_BIT_MASK);
             return res;
         }
     }
@@ -121,11 +100,10 @@ uint32_t ldr_a(char** str){
 }
 
 uint32_t str_a(char** str){
-    uint32_t cond;
     uint32_t i_bit = 1 << 25;
     uint32_t p_bit;
     uint32_t u_bit;
-    uint32_t l_bit = 0 << 20;
+    uint32_t l_bit = 0 << L_BIT_INDEX;
     //char* address = str[2];
     char *end;
     if (str[3] == NULL){
@@ -138,17 +116,16 @@ uint32_t str_a(char** str){
         uint32_t offset;
         if (offset_s >= 0){
             offset = offset_s;
-            u_bit = 1 << 23;
+            u_bit = 1 << UP_BIT_INDEX;
         } else {
             offset = offset_s;
-            u_bit = 0 << 23;
+            u_bit = 0 << UP_BIT_INDEX;
         }
-        cond = 0xE << 28;
-        p_bit = 1 << 24;
+        p_bit = 1 << P_BIT_INDEX;
         int rd = atoi(str[1] + 1);
         rd <<= 12;
         uint32_t rd_mask = *(uint32_t*)&rd;
-        uint32_t res = (p_bit | i_bit | cond | u_bit | l_bit | rn | rd_mask | offset | (1 << 26));
+        uint32_t res = (p_bit | i_bit | COND_MASK | u_bit | l_bit | rn | rd_mask | offset | STANDARD_BIT_MASK);
         return res;
     } else {
         if ((end = strstr(str[3], "]")) != NULL){
@@ -156,9 +133,8 @@ uint32_t str_a(char** str){
                 // Pre-indexed with offset
                 char* rn_str = str[2] + 2;
                 uint32_t rn = atoi(rn_str) << 16;
-                p_bit = 1 << 24;
-                u_bit = 1 << 23;
-                cond = 0xE << 28;
+                p_bit = 1 << P_BIT_INDEX;
+                u_bit = 1 << UP_BIT_INDEX;
                 char* offset_str = str[3];
                 offset_str[strlen(offset_str) - 2] = '\0';
                 // printf("Offset: %s", offset_str);
@@ -168,20 +144,19 @@ uint32_t str_a(char** str){
                 rd <<= 12;
                 i_bit = 0 << 25;
                 uint32_t rd_mask = *(uint32_t*)&rd;
-                uint32_t res = (p_bit | i_bit | cond | u_bit | l_bit | rn | rd_mask | offset | (1 << 26));
+                uint32_t res = (p_bit | i_bit | COND_MASK | u_bit | l_bit | rn | rd_mask | offset | STANDARD_BIT_MASK);
                 return res;
             }  else {
             // Post-indexed
                 uint32_t offset = atoi(str[3]);
                 char* rn_str = str[2] + 2;
                 uint32_t rn = atoi(rn_str) << 16;
-                cond = 0xE << 28;
-                p_bit = 0 << 24;
-                u_bit = 1 << 23;
+                p_bit = 0 << P_BIT_INDEX;
+                u_bit = 1 << UP_BIT_INDEX;
                 int rd = atoi(str[1] + 1);
                 rd <<= 12;
                 uint32_t rd_mask = *(uint32_t*)&rd;
-                uint32_t res = (p_bit | i_bit | cond | u_bit | l_bit | rn | rd_mask | offset | (1 << 26));
+                uint32_t res = (p_bit | i_bit | COND_MASK | u_bit | l_bit | rn | rd_mask | offset | STANDARD_BIT_MASK);
                 return res;
         }
     }
--- src/single_data_transfer_assemble.h
+++ src/single_data_transfer_assemble.h
@@ -6,6 +6,15 @@
 #include <string.h>
 #include "data_processing_assemble.h"
 #include "assemble_utilities.h"
+#define UP_BIT_INDEX 23
+#define L_BIT_INDEX 20
+#define P_BIT_INDEX 24
+#define STANDARD_BIT 1
+#define STANDARD_BIT_INDEX 26
+#define STANDARD_BIT_MASK (STANDARD_BIT << STANDARD_BIT_INDEX)
+#define COND_VALUE 0xE
+#define COND_INDEX 28
+#define COND_MASK (COND_VALUE << COND_INDEX)
 
 uint32_t ldr_a(char** address);
 
