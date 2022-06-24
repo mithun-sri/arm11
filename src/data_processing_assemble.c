c85387dc592662c07a91506f8ba449bf3f8d31bb
Mithun Sri <ms2921@vm-shell3.doc.ic.ac.uk>
2022-06-24 12:30:47 +0100

    Removed unneccessary print statements

--- src/data_processing_assemble.c
+++ src/data_processing_assemble.c
@@ -1,13 +1,10 @@
 #include "data_processing_assemble.h"
-#include "data_processing.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 uint32_t add_a(uint8_t rn, uint8_t rd, char* op2) {
-	printf("Opcode: %s", op2);
   uint8_t val;
-  printf("%s\n", op2);
   if (strncmp(op2, "0x", 2) == 0){
     // Hex operand
 	  sscanf((op2 + 2), "%hhx", &val);
@@ -21,14 +18,10 @@ uint32_t add_a(uint8_t rn, uint8_t rd, char* op2) {
     sscanf(op2, "%hhx", &val);
     return (COND_CODE | (1 << I_BIT_OFFSET)| (rn << RN_OFFSET) |(0x4 << OPCODE_OFFSET) | (rd << RD_OFFSET) | val);
   }
-
-  //return (COMMON_DATA_PROCESSING_BITS + (ADD_OPCODE << OPCODE_OFFSET)
-  //+ (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
 uint32_t sub_a(uint8_t rn, uint8_t rd, char* op2) {
   uint8_t val;
-  printf("%s\n", op2);
   if (strncmp(op2, "0x", 2) == 0){
     // Hex operand
 	  sscanf((op2 + 2), "%hhx", &val);
@@ -38,18 +31,14 @@ uint32_t sub_a(uint8_t rn, uint8_t rd, char* op2) {
       sscanf((op2 + 1), "%hhx", &val);
       return ((0xE << 28) | (rn << 16) | (0x2 << 21) |(rd << 12) | val);
   } else {
+    // Immediate operand
     sscanf(op2, "%hhx", &val);
     return ((0xE << 28) | (1 << 25)| (rn << 16) |(0x2 << 21) | (rd << 12) | val);
-    // Immediate operand
-
-  // return (COMMON_DATA_PROCESSING_BITS + (SUB_OPCODE << OPCODE_OFFSET) 
-  // + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 }
 
 uint32_t rsb_a(uint8_t rn, uint8_t rd, char* op2) {
   uint8_t val;
-  printf("%s\n", op2);
   if (strncmp(op2, "0x", 2) == 0){
     // Hex operand
 	  sscanf((op2 + 2), "%hhx", &val);
@@ -59,18 +48,14 @@ uint32_t rsb_a(uint8_t rn, uint8_t rd, char* op2) {
       sscanf((op2 + 1), "%hhx", &val);
       return ((0xE << 28) | (rn << 16) | (0x3 << 21) |(rd << 12) | val);
   } else {
+     // Immediate operand
     sscanf(op2, "%hhx", &val);
     return ((0xE << 28) | (1 << 25)| (rn << 16) |(0x3 << 21) | (rd << 12) | val);
-    // Immediate operand
   }
-  // return (COMMON_DATA_PROCESSING_BITS + (RSB_OPCODE << OPCODE_OFFSET) 
-  // + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
 uint32_t and_a(uint8_t rn, uint8_t rd, char *op2) {
-  printf("Opcode: %s", op2);
   uint8_t val;
-  printf("%s\n", op2);
   if (strncmp(op2, "0x", 2) == 0){
     // Hex operand
 	  sscanf((op2 + 2), "%hhx", &val);
@@ -80,19 +65,14 @@ uint32_t and_a(uint8_t rn, uint8_t rd, char *op2) {
       sscanf((op2 + 1), "%hhx", &val);
       return ((0xE << 28) | (rn << 16) |(rd << 12) | val);
   } else {
+    // Immediate operand
     sscanf(op2, "%hhx", &val);
     return ((0xE << 28) | (1 << 25)| (rn << 16) | (rd << 12) | val);
-    // Immediate operand
   }
-  
-  // return (COMMON_DATA_PROCESSING_BITS + (AND_OPCODE << OPCODE_OFFSET)
-  // + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
 uint32_t eor_a(uint8_t rn, uint8_t rd, char* op2) {
-  printf("Opcode: %s", op2);
   uint8_t val;
-  printf("%s\n", op2);
   if (strncmp(op2, "0x", 2) == 0){
     // Hex operand
 	  sscanf((op2 + 2), "%hhx", &val);
@@ -102,18 +82,14 @@ uint32_t eor_a(uint8_t rn, uint8_t rd, char* op2) {
       sscanf((op2 + 1), "%hhx", &val);
       return ((0xE << 28) | (rn << 16) | (0x1 << 21) |(rd << 12) | val);
   } else {
+    // Immediate operand
     sscanf(op2, "%hhx", &val);
     return ((0xE << 28) | (1 << 25)| (rn << 16) | (0x1 << 21) | (rd << 12) | val);
-    // Immediate operand
   }
-  // return (COMMON_DATA_PROCESSING_BITS + (EOR_OPCODE << OPCODE_OFFSET) 
-  // + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
 uint32_t orr_a(uint8_t rn, uint8_t rd, char* op2) {
-  printf("Opcode: %s", op2);
   uint8_t val;
-  printf("%s\n", op2);
   if (strncmp(op2, "0x", 2) == 0){
     // Hex operand
 	  sscanf((op2 + 2), "%hhx", &val);
@@ -123,12 +99,10 @@ uint32_t orr_a(uint8_t rn, uint8_t rd, char* op2) {
       sscanf((op2 + 1), "%hhx", &val);
       return ((0xE << 28) | (rn << 16) | (0xC << 21) |(rd << 12) | val);
   } else {
+    // Immediate operand
     sscanf(op2, "%hhx", &val);
     return ((0xE << 28) | (1 << 25)| (rn << 16) | (0xC << 21) | (rd << 12) | val);
-    // Immediate operand
   }
-  // return (COMMON_DATA_PROCESSING_BITS + (ORR_OPCODE << OPCODE_OFFSET) 
-  // + (rn << RN_OFFSET) + (rd << RD_OFFSET) + op2);
 }
 
 static uint32_t convertOperand(uint32_t op2_32bit) {
@@ -164,15 +138,10 @@ static uint32_t convertOperand(uint32_t op2_32bit) {
 }
 
 uint32_t mov_a(uint8_t rd, char* op2) { 
-	printf("Mov op2: %s", op2);
   uint32_t val;
-  // uint32_t res = val;
-  
-  printf("%s\n", op2);
   if (strncmp(op2, "0x", 2) == 0){
     // Hex operand
 	  sscanf((op2 + 2), "%x", &val);
-    printf("VAL: %x\n", val);
     uint32_t value;
     if (val > 255) {
       value = convertOperand(val);
@@ -185,26 +154,20 @@ uint32_t mov_a(uint8_t rd, char* op2) {
       sscanf((op2 + 1), "%x", &val);
       return ((0xE << 28) | (0xD << 21) | (rd << 12) | val);
   } else {
-    printf("%s\n", op2);
     val = atoll(op2);
-    printf("%x\n", val);
     uint32_t value;
     if (val > 255) {
       value = convertOperand(val);
     } else {
       value = val;
     }
-    printf("%x\n", value);
     return ((0xE << 28) | (1 << 25) | (0xD << 21) | (rd << 12) | value);
     // Immediate operand
   }
-      	// return (COMMON_DATA_PROCESSING_BITS + (MOV_OPCODE << OPCODE_OFFSET) 
-  // + (rd << RD_OFFSET) + val);
 }
 
 uint32_t tst_a(uint8_t rn, char* op2) { 
   uint8_t val;
-  printf("%s\n", op2);
   if (strncmp(op2, "0x", 2) == 0){
     // Hex operand
 	  sscanf((op2 + 2), "%hhx", &val);
@@ -214,18 +177,14 @@ uint32_t tst_a(uint8_t rn, char* op2) {
       sscanf((op2 + 1), "%hhx", &val);
       return ((0xE << 28) | (0x8 << 21) | (rn << 16) | val);
   } else {
+    // Immediate operand
     sscanf(op2, "%hhx", &val);
     return ((0xE << 28) | (1 << 25) | (0x8 << 21) | (rn << 16) | val);
-    // Immediate operand
   }
-
-  // return (COMMON_DATA_PROCESSING_BITS + (TST_OPCODE << OPCODE_OFFSET) 
-  // + S_BIT_MASK + (rn << RN_OFFSET) + op2); 
 }
 
 uint32_t teq_a(uint8_t rn, char* op2) {
   uint8_t val;
-  printf("%s\n", op2);
   if (strncmp(op2, "0x", 2) == 0){
     // Hex operand
 	  sscanf((op2 + 2), "%hhx", &val);
@@ -235,17 +194,14 @@ uint32_t teq_a(uint8_t rn, char* op2) {
       sscanf((op2 + 1), "%hhx", &val);
       return ((0xE << 28) | (0x9 << 21) | (rn << 16) | val);
   } else {
+    // Immediate operand
     sscanf(op2, "%hhx", &val);
     return ((0xE << 28) | (1 << 25) | (0x9 << 21) | (rn << 16) | val);
-    // Immediate operand
   }
-  // return (COMMON_DATA_PROCESSING_BITS + (TEQ_OPCODE << OPCODE_OFFSET) 
-  // + S_BIT_MASK + (rn << RN_OFFSET) + op2); 
 }
 
 uint32_t cmp_a(uint8_t rn, char* op2) {
   uint8_t val;
-  printf("%s\n", op2);
   if (strncmp(op2, "0x", 2) == 0){
     // Hex operand
 	  sscanf((op2 + 2), "%hhx", &val);
@@ -255,10 +211,8 @@ uint32_t cmp_a(uint8_t rn, char* op2) {
       sscanf((op2 + 1), "%hhx", &val);
       return ((0xE << 28) | (0xA << 21) | (1 << 20) | (rn << 16) | val);
   } else {
+    // Immediate operand
     sscanf(op2, "%hhx", &val);
     return ((0xE << 28) | (1 << 25) | (0xA << 21) | (1 << 20) | (rn << 16) | val);
-    // Immediate operand
   }
-  // return (COMMON_DATA_PROCESSING_BITS + (CMP_OPCODE << OPCODE_OFFSET) 
-  // + S_BIT_MASK + (rn << RN_OFFSET) + op2);
 }
--- src/special_assemble.c
+++ src/special_assemble.c
@@ -5,20 +5,15 @@
 #include <string.h>
 
 uint32_t lsl_a(char** str) {
-     for (int i = 0; str[i]; i++){
-        printf("Str: %s\n", str[i]);
-     }
-     
-     uint32_t int_amt;
+   uint32_t int_amt;
      if ((strncmp(str[2], "0x", 2)) == 0){
         // Hex
         int_amt = strtol(str[2] + 2, NULL, 16);
      } else {
+        // Base 10
         int_amt = atoi(str[2]);
      }
-     
      uint32_t rm = atoi(str[1] + 1);
-     // return 0;
      return (COND << COND_OFFSET) + (MOV_OPCODE << OPCODE_OFFSET) 
     + (rm << RD_OFFSET) + (int_amt << INTEGER_SHIFT) + rm;
 }
